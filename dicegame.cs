6e6f6622fbf17762aecf774b0690d95233ad6bc4
Breno Pinheiro de Moura <143467828+BrenoIFCE@users.noreply.github.com>
2025-03-17 16:52:36 -0300

    dicegame.cs
    Pontuação atual tirada das condicionais If

--- dicegame.cs
+++ dicegame.cs
@@ -16,15 +16,14 @@ internal class Program
         {
             Console.WriteLine("Você girou 3 dados iguais, bônus de 6 pontos!");
             total += 6;
-            Console.WriteLine($"Sua pontuação atual: {total} pontos.");
+            
         }
         else if (giro1 == giro2 || giro2 == giro3 || giro3 == giro1)
         {
             Console.WriteLine("Você girou 2 dados iguais, bônus de 2 pontos!");
             total += 2;
-            Console.WriteLine($"Sua pontuação atual: {total} pontos.");
         }
-
+        Console.WriteLine($"Sua pontuação atual: {total} pontos.");
         if (total >= 16) Console.Write("Parabéns, você ganhou um carro!");
         else if(total >= 10) Console.Write("Parabéns, você ganhou um laptop!");
         else if(total == 7) Console.Write("Parabéns, você ganhou uma viagem para 2 pessoas!");
